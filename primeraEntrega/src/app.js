426d0bffd6a728e9f5c3a08b8c3cc9ed344e7273
Anna Gómez <114868178+APEGS@users.noreply.github.com>
2024-08-26 11:38:20 -0300

    Add files via upload

---
+++ primeraEntrega/src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import productsRouter from './routes/products.router.js';
+import cartsRouter from './routes/carts.router.js';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT,()=>console.log(`Listening on ${PORT}`));
+
+app.use(express.json());
+app.use('/api/products',productsRouter);
+app.use('/api/carts',cartsRouter);
