b6160e31f829e4001c4189eaff1babcd696d4f55
Anna Gómez <114868178+APEGS@users.noreply.github.com>
2024-08-26 21:52:48 -0300

    primeraEntrega

---
+++ primeraEntrega/src/routes/products.router.js
@@ -0,0 +1,116 @@
+import { Router } from 'express';
+import ProductManager from '../manager/ProductManager.js';
+
+const productsRouter = Router();
+const productManager = new ProductManager();
+
+const newProducts = [{
+    "id":70,
+   "title": "Silla Negro G",
+   "description":"Precio por unidad",
+   "code":"19",
+   "price":7,
+   "estado":true,
+   "stock":1,
+   "category":"adult"
+},
+{
+    "id":40,
+   "title": "Silla Beige P",
+   "description":"Precio por unidad",
+   "code":"40",
+   "price":2,
+   "estado":true,
+   "stock":1,
+   "category":"kids"
+}
+];
+
+//---------1 obtiene la lista de productos
+//---------1 obtiene la lista de productos
+productsRouter.get('/',async (req,res)=>{
+    try {
+        const products = await productManager.getProducts();
+        res.send({products,newProducts});
+    } catch (error) {
+        console.log('error');
+        res.send('error: not finding products');
+    }
+});
+
+//---------2 obtiene el producto por medio de su id
+//---------2 obtiene el producto por medio de su id
+productsRouter.get('/:pid',async(req,res)=>{
+    try {
+        const {pid} = req.params;
+        const parsedId = parseInt(pid);
+        const product = await productManager.getProductById(parsedId);
+        if(!product){
+            return res.send('product not found');
+        }
+        res.send({product});
+    } catch (error) {
+        console.log('error');
+        res.send('error: cannot find product');
+    }
+});
+
+//---------3 agrega nuevo producto
+//---------3 agrega nuevo producto
+productsRouter.post('/',async(req,res)=>{
+    const {title,description,code,price,stock,category}= req.body
+    if(!title || !description || !code || !price || !stock || !category){
+        return res.status(400).send('product information is incomplete');
+    };
+    const newProduct = {
+        title,
+        description,
+        code,
+        price,
+        estado: true,
+        stock,
+        category
+    };
+    if(newProducts.length===0){
+        newProduct.id = 1
+    }else{
+        newProduct.id = newProducts[newProduct.length-1].id + 1;
+    }
+    newProducts.push(newProduct);
+    //const product = await productManager.addCreatedProduct(newProduct);
+    res.sendStatus(201);
+    console.log(newProduct);
+});
+
+//---------4 actualiza producto por medio de su id
+//---------4 actualiza producto por medio de su id
+productsRouter.put('/:pid',async(req,res)=>{
+    const {pid} = req.params;
+    const {title,description,price,category} = req.body;
+    const parsedId = parseInt(pid);
+    const productIndex = newProducts.findIndex(p=>p.id===parsedId);
+    console.log(newProducts);
+    if(productIndex === -1){
+        console.log('no');
+        return res.status(400).send({status:"error",error:"Product doesn't exist"})
+    }
+    // const product = await productManager.getProductById(pid);
+    // const arr = await productManager.updateProduct();
+    newProducts[productIndex] = {...newProducts[productIndex],title,description,price,category};
+    console.log(newProducts);
+    res.send({status:"success",message:"Product updated"});
+});
+
+//---------5 elimina un producto por medio de su id
+//---------5 elimina un producto por medio de su id
+// productsRouter.delete('/:pid',async(req,res)=>{
+//     const {pid} = req.params;
+//     const productIndex = newProducts.findIndex(u=>u.id===pid);
+//     if(productIndex === -1){
+//         return res.status(400).send({status:"error",error:"User doesn't exist"})
+//     }
+//     newProducts.splice(productIndex,1);
+//     res.sendStatus(204);
+// });
+
+export default productsRouter;
