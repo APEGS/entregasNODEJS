e57114e9bec4a7497cf3141b591fe2abcaa00f80
Anna Gómez <114868178+APEGS@users.noreply.github.com>
2024-10-02 10:36:29 -0300

    terceraEntrega

---
+++ terceraEntrega/src/routes/carts.router.js
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import Cart from "../models/carts.model.js";
+
+const cartsRouter = Router();
+
+cartsRouter.put('/:cid',async(req,res)=>{});
+
+cartsRouter.put('/:cid/products/:pid',async(req,res)=>{
+    const { page = 1} = req.query;
+    const carts = await Product.paginate({},{page});
+    const result = {
+        payload:carts.docs,
+        nextPage:carts.nextPage,
+        prevPage:carts.prevPage,
+        hasNextPage:carts.hasNextPage,
+        hasPrevPage:carts.hasNextPage,
+        nextLink: `/api/carts/${carts.nextPage}`,
+        prevLink: `/api/carts/${carts.prevLink}`
+    };
+    res.json(result);
+});
+
+cartsRouter.delete('/:cid',async(req,res)=>{
+    const {cid} = req.params;
+    const parsedId = parseInt(cid);
+    const findCart = await Cart.find(parsedId).explain('executionstats');
+    if(findCart === -1){
+        return res.status(400).send({status:"error",error:"Cart doesn't exist"})
+    }
+    await productManager.findArr(newProducts);
+    Cart.splice(findCart,1);
+    console.log("you've elimitated your cart");
+    res.sendStatus(204);
+});
+
+export default cartsRouter;
