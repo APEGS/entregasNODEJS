95afe0dd71be65e38f0ee2fbf240e722f45cc439
Anna Gómez <114868178+APEGS@users.noreply.github.com>
2024-12-28 08:48:25 -0300

    entrega2

---
+++ segundaEntrega2/src/routes/userRouter.js
@@ -0,0 +1,191 @@
+import { Router } from "express";
+import userManager from "../dao/users.manager.js";
+import { uploader } from '../uploader.js';
+import passport from 'passport';
+import initAuthStrategies from "../auth/passport.config.js";
+import { createToken, verifyToken } from "../utils.js";
+import cookieRouter from './cookieRouter.js';
+import config from "../config.js";
+
+const userRouter = Router();
+const manager = new userManager();
+const cookieSign = cookieRouter();
+
+initAuthStrategies();
+
+export const auth = (req, res, next) => {
+    if (req.session?.userData || req.session?.passport.user) {
+        next();
+    } else {
+        res.status(401).send({ error: 'Not authorized', data: [] });
+    }
+};
+
+const getJWT = (req,res)=>{}
+
+userRouter.get('/', async (req, res) => {
+    try {
+        const data = await manager.get();
+        res.status(200).send({ error: null, data: data });
+    } catch (err) {
+        res.status(500).send({ error: 'Error interno de ejecución del servidor', data: [] });
+    }
+});
+
+userRouter.post('/', auth, uploader.single('thumbnail'), async (req, res) => { 
+    try {
+        const { name, age, email } = req.body;
+
+        if (name != '' && age != '' && email != '') {
+            const data = { name: name, age: +age, email: email };
+            const process = await manager.add(data);
+            res.status(200).send({ error: null, data: process });
+        } else {
+            res.status(400).send({ error: 'Faltan campos obligatorios', data: [] });
+        }
+    } catch (err) {
+        res.status(500).send({ error: 'Error interno de ejecución del servidor', data: [] });
+    }
+});
+
+userRouter.patch('/:id?', auth, async (req, res) => {
+    try {
+        const id = req.params.id;
+        
+        if (!id) {
+            res.status(400).send({ error: 'Param id required', data: null });
+        } else {
+            const { name, age, email } = req.body;
+            const filter = { _id: id };
+            const update = {};
+            if (name) update.name = name;
+            if (age) update.age = +age;
+            if (email) update.email = email;
+            const options = { new: true };
+            
+            const process = await manager.update(filter, update, options);
+            if (!process) {
+                res.status(404).send({ error: 'User not found', data: [] });
+            } else {
+                res.status(200).send({ error: null, data: process });
+            }
+        }
+    } catch (err) {
+        res.status(500).send({ error: 'Error interno de ejecución del servidor', data: [] });
+    }
+});
+
+router.delete('/:id?', verifyToken, handlePolicies(['ADMIN']), async (req, res) => {
+    try {
+        const id = req.params.id;
+        
+        if (!id) {
+            res.status(400).send({ error: 'Se requiere parámetro id', data: null });
+        } else {
+            const filter = { _id: id };
+            const options = {};
+            
+            const process = await manager.delete(filter, options);
+            if (!process) {
+                res.status(404).send({ error: 'No se encuentra el usuario', data: [] });
+            } else {
+                res.status(200).send({ error: null, data: process });
+            }
+        }
+    } catch (err) {
+        res.status(500).send({ error: 'Error interno de ejecución del servidor', data: [] });
+    }
+});
+
+userRouter.post('/register', async (req, res) => {
+    const { firstname, lastname, username, password } = req.body;
+
+    if (firstname != '' && lastname != '' && username != '' && password != '') {
+        const process = await manager.register({ firstName: firstname, lastName: lastname, email: username, password: bcrypt.hashSync(password, saltRounds) });
+        if (process) {
+            res.status(200).send({ error: null, data: 'Usuario registrado' });
+        } else {
+            res.status(401).send({ error: 'Ya existe un usuario con ese email', data: [] });
+        }
+    } else {
+        res.status(400).send({ error: 'Faltan campos obligatorios', data: [] });
+    }
+});
+
+userRouter.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    if (username != '' && password != '') {
+        const process = await manager.authenticate(username, password);
+        if (process) {
+            req.session.userData = { firstName: process.firstName, lastName: process.lastName, email: process.email, admin: true };
+
+            req.session.save(err => {
+                if (err) return res.status(500).send({ error: 'Error al almacenar datos de sesión', data: [] });
+                res.redirect('/views/profile');
+            });
+        } else {
+            res.status(401).send({ error: 'Usuario o clave no válidos', data: [] });
+        }
+    } else {
+        res.status(400).send({ error: 'Faltan campos: obligatorios username, password', data: [] });
+    }
+});
+
+userRouter.post('/pplogin', passport.authenticate('login', { failureRedirect: '/views/login' }), async (req, res) => {
+    req.session.save(err => {
+        if (err) return res.status(500).send({ error: 'Error al almacenar datos de sesión', data: [] });
+        res.redirect('/views/profile');
+    });
+});
+
+userRouter.get('/ghlogin', passport.authenticate('ghlogin', { scope: ['user:email'] }), async (req, res) => {});
+
+userRouter.get('/githubcallback', passport.authenticate('ghlogin', { failureRedirect: '/views/login' }), async (req, res) => {
+    req.session.save(err => {
+        if (err) return res.status(500).send({ error: 'Error al almacenar datos de sesión', data: [] });
+        res.redirect('/views/profile');
+    });
+});
+
+userRouter.post('/jwtlogin', async (req, res) => {
+    const { username, password } = req.body;
+
+    if (username != '' && password != '') {
+        const process = await manager.authenticate(username, password);
+        if (process) {
+            const payload = { username: username, admin: true };
+            const token = createToken(payload, '1h');
+            res.status(200).send({ error: null, data: { autentication: 'ok', token: token }});
+        } else {
+            res.status(401).send({ error: 'Usuario o clave no válidos', data: [] });
+        }
+    } else {
+        res.status(400).send({ error: 'Faltan campos: obligatorios username, password', data: [] });
+    }
+});
+
+userRouter.get('/current',passport.authenticate('jwt',{session:false}),(req,res)=>{
+    res.send(req.user);
+});
+
+userRouter.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) return res.status(500).send({ error: 'Error al cerrar sesión', data: [] });
+        res.redirect('/views/login');
+    });
+});
+
+userRouter.get('/private', auth, (req, res) => {
+    res.status(200).send({ error: null, data: 'Este contenido solo es visible por usuarios autenticados' });
+});
+
+userRouter.get('/private2', verifyToken, (req, res) => {
+    res.status(200).send({ error: null, data: 'Este contenido solo es visible por usuarios autenticados' });
+});
+
+userRouter.get('/:cid/purchase',passport.authenticate('jwt',{session:false}),(req,res)=>{
+    res.send(req.user);
+});
+
+export default userRouter;
